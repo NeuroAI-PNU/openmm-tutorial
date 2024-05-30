7c03d2bf5e84e25b5412396b1f5407834130f99b
hyunsu_HP <hyunspookey@gmail.com>
2024-05-30 10:58:06 +0900

    add comment for 1st example

--- User guide/First example.py
+++ User guide/First example.py
@@ -4,17 +4,31 @@ from openmm.unit import *
 from sys import stdout
 from datetime import datetime
 
+# http://docs.openmm.org/latest/userguide/application/02_running_sims.html#a-first-example
+
 tik = datetime.now()
 pdb = PDBFile('input.pdb')
+
 forcefield = ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
-system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
-        nonbondedCutoff=1*nanometer, constraints=HBonds)
+
+system = forcefield.createSystem(pdb.topology, 
+                                 nonbondedMethod=PME, # particle mesh ewald fro long range electrostatics interaction
+                                 nonbondedCutoff=1*nanometer, #1 nm (10 angstrom) cutoff for nonbonded interactions
+                                 constraints=HBonds) # all bonds involving hydrogen are constrained
+
+# for Langevin dynamics
+# Temperature, friction coefficient, time step
 integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.004*picoseconds)
+
+# for CUDA
 platform = Platform.getPlatformByName('CUDA')
+
+
 simulation = Simulation(pdb.topology, system, integrator, platform)
 simulation.context.setPositions(pdb.positions)
-simulation.minimizeEnergy()
-simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.minimizeEnergy() # minimize energy at a starting point
+simulation.reporters.append(PDBReporter('output.pdb', 
+                                        1000)) # every 1000 steps, write the coordinates to a file
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
         potentialEnergy=True, temperature=True))
 simulation.step(10000)
